971b334b910236ed67c01d49826bdc2970a536e2
Valeri Zanguri <zanguraa@gmail.com>
2023-06-12 01:49:06 +0400

    created searchbar component with two search input

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div>
+      <input placeholder="Search Businesses" />
+      <input placeholder="Where?" />
+      <button>Let's Go</button>
+    </div>
+  );
+};
+
+export default SearchBar;
